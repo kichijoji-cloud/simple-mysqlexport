c14ccda5a4856f14f94a9a4a6295449e906f95db
git1 <git1@to.matrix.jp>
2023-12-23 16:43:42 +0900

    update mysqlexport.py

--- mysqlexport.py
+++ mysqlexport.py
@@ -3,7 +3,9 @@ import mysql.connector
 import csv
 import json
 
-def export_to_tsv(username, password, host, db_name, table_name, columns, output_file, fields_enclosed_by=None):
+
+
+def export_to_tsv(username, password, host, db_name, table_name, columns, output_file, fields_enclosed_by=None, delim="\t"):
 	# MySQLに接続
 	connection = mysql.connector.connect(
 		user=username,
@@ -17,19 +19,30 @@ def export_to_tsv(username, password, host, db_name, table_name, columns, output
 	# カラムをカンマ区切りの文字列に変換
 	columns_str = ', '.join(columns)
 
+	if(fields_enclosed_by==None or fields_enclosed_by==""):
+		fields_enclosed_by='\"'
+
 	# クエリの作成
-	query = f"SELECT {columns_str} FROM {table_name}"
+	query = f"SELECT {columns_str} FROM {table_name} " +\
+	" where post_status = 'publish' " +\
+	" AND   post_type = 'xo_event' " +\
+	""
 
 	# クエリの実行
 	cursor.execute(query)
 
 	# 結果を取得
 	results = cursor.fetchall()
-
+   
 	# TSVファイルに書き込み
 	with open(output_file, 'w', newline='', encoding='utf-8') as tsvfile:
-		tsv_writer = csv.writer(tsvfile, delimiter='\t', quotechar=fields_enclosed_by, quoting=csv.QUOTE_MINIMAL)
-
+		tsv_writer=None
+		if(fields_enclosed_by==None or fields_enclosed_by==""):
+			tsv_writer = csv.writer(tsvfile, delimiter=delim, quoting=csv.QUOTE_MINIMAL)
+		else:
+			tsv_writer = csv.writer(tsvfile, delimiter=delim, quotechar=fields_enclosed_by, quoting=csv.QUOTE_ALL)
+		
+		#print("fields_enclosed_by: " + fields_enclosed_by)
 		# カラム名を書き込み
 		tsv_writer.writerow(columns)
 
@@ -46,8 +59,8 @@ def load_config_from_json(config_file):
 	return config
 
 def main():
-	parser = argparse.ArgumentParser(description='Export MySQL table to TSV file')
-	parser.add_argument('--config', help='JSON config file with MySQL connection details and export options')
+	parser = argparse.ArgumentParser(description='Export MySQL table to TSV(\\t delimiter) file')
+	parser.add_argument('--config', help='JSON config file with MySQL connection details and export TSV(\\t) options')
 
 	args, remaining_args = parser.parse_known_args()
 
@@ -62,6 +75,7 @@ def main():
 		parser.add_argument('--columns', nargs='+', required=True, help='Columns to export')
 		parser.add_argument('--output', required=True, help='Output file name')
 		parser.add_argument('--fields-enclosed-by', help='Character to enclose fields')
+		parser.add_argument('--delimiter', help='delimiter default \\t(tsv)')
 
 		config = vars(parser.parse_args(remaining_args))
 
@@ -73,10 +87,12 @@ def main():
 		config['table'],
 		config['columns'],
 		config['output'],
-		config.get('fields_enclosed_by')
+		config['fields_enclosed_by'],
+		config['delimiter']
 	)
 
 if __name__ == "__main__":
 	main()
+
 
 
